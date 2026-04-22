779d6e945a5b0761a244835a625f2da3f05ec9fe
LusellyHinestroza <valencialucelly48@gmail.com>
2026-04-22 12:27:29 -0500

    declaracion de datos

---
+++ src/Parte2/Cafeteria.java
@@ -0,0 +1,16 @@
+package Parte2;
+
+public class Cafeteria {
+    public static void main(String[] args){
+        String[] nombres = {"Café americano", "Capuchino", "Jugo naranja", "Tostada",
+                "Brownie"};
+
+        double[] precios = {3500, 5000, 4000, 4500, 6000};
+
+        char[] categorias = {'B', 'B', 'F', 'C', 'P'}; // B=Bebida caliente, F=Fría, C=Comida,P=Postre
+
+        int[] pedidoProducto = {0, 2, 1, 4, 0, 3}; // índice del producto en cada pedido
+
+        int[] pedidoCantidad = {2, 1, 3, 1, 1, 2}; // unidades por pedido
+    }
+}
